b1ffa05e6ff48bd9dca65eedc608617fa920dcb8
Luis Eduardo <luisedu.soares@outlook.com>
2018-12-13 01:52:27 -0200

    commit

--- app.py
+++ app.py
@@ -11,18 +11,15 @@ db.create_all()
 
 @app.route('/')
 def index():
-    return 'nada'
-
-
-if __name__ == '__main__':
     from db.users import Users
     test = Users(username='admin', password='admin', enrolment='admin', email='admin')
     db.session.add(test)
     db.session.commit()
-
-    print('\n\n\n\n')
+    response = ''
     for i in Users.query.all():
-        print(f'{i.id} - {i.username} - {i.enrolment} - {i.email}')
-    print('\n\n\n\n')
+        response += f'{i.id} - {i.username} - {i.enrolment} - {i.email}'
+    return response
 
+if __name__ == '__main__':
+    __import__('time').sleep(10)
     app.run(host='0.0.0.0')
