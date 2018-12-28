339302e439a01a4891a2e36a3e4408077dea6eb1
Luis Eduardo <luisedu.soares@outlook.com>
2018-12-28 14:27:51 -0200

    initial commit master local

--- app/__init__.py
+++ app/__init__.py
@@ -40,7 +40,7 @@ def not_found(error):
 
 with app.app_context():
     # remove this in production - start remove
-    #bluep_db.drop_all()
+    # bluep_db.drop_all()
     print(' * Drop all tables!')
     # end remove
     bluep_db.create_all()
